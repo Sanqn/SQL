aa59b4dbbcead18433ebd4c86f9197b544067d95
Sanqn <sandisvert@gmail.com>
2022-06-06 23:03:43 +0300

    Mysql insert values

--- SQLBD.py
+++ SQLBD.py
@@ -121,16 +121,85 @@ connection = create_connection("localhost", "admin", "root", "new_app")
 # # execute_query(connection, create_comments_table)
 
 
-def execute_query_users(connection):
+# def execute_query_users(connection):
+#     cursor = connection.cursor()
+#     try:
+#         sql = "INSERT INTO users (name, age, gender, nationality) VALUES ( %s, %s, %s, %s )"
+#         val = [('James', 25, 'male', 'USA'),
+#                ('Leila', 32, 'female', 'France'),
+#                ('Brigitte', 35, 'female', 'England'),
+#                ('Mike', 40, 'male', 'Denmark'),
+#                ('Elizabeth', 21, 'female', 'Canada')
+#                ]
+#
+#         cursor.executemany(sql, val)
+#         connection.commit()
+#         print("Query executed successfully")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#
+# execute_query_users(connection)
+
+
+# def execute_query_posts(connection):
+#     cursor = connection.cursor()
+#     try:
+#         sql = "INSERT INTO posts (title, description, user_id) VALUES ( %s, %s, %s )"
+#         val = [
+#             ("Happy", "I am feeling very happy today", 1),
+#             ("Hot Weather", "The weather is very hot today", 2),
+#             ("Help", "I need some help with my work", 2),
+#             ("Great News", "I am getting married", 1),
+#             ("Interesting Game", "It was a fantastic game of tennis", 5),
+#             ("Party", "Anyone up for a late-night party today?", 3)
+#         ]
+#
+#         cursor.executemany(sql, val)
+#         connection.commit()
+#         print("Query executed successfully")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#
+# execute_query_posts(connection)
+
+# def execute_query_comments(connection):
+#     cursor = connection.cursor()
+#     try:
+#         sql = "INSERT INTO comments (text, user_id, post_id) VALUES ( %s, %s, %s )"
+#         val = [
+#             ('Count me in', 1, 6),
+#             ('What sort of help?', 5, 3),
+#             ('Congrats buddy', 2, 4),
+#             ('I was rooting for Nadal though', 4, 5),
+#             ('Help with your thesis?', 2, 3),
+#             ('Many congratulations', 5, 4)
+#         ]
+#
+#         cursor.executemany(sql, val)
+#         connection.commit()
+#         print("Query executed successfully")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#
+# execute_query_comments(connection)
+
+
+def execute_query_likes(connection):
     cursor = connection.cursor()
     try:
-        sql = "INSERT INTO users (name, age, gender, nationality) VALUES ( %s, %s, %s, %s )"
-        val = [('James', 25, 'male', 'USA'),
-               ('Leila', 32, 'female', 'France'),
-               ('Brigitte', 35, 'female', 'England'),
-               ('Mike', 40, 'male', 'Denmark'),
-               ('Elizabeth', 21, 'female', 'Canada')
-               ]
+        sql = "INSERT INTO likes (user_id, post_id) VALUES ( %s, %s )"
+        val = [
+            (1, 6),
+            (2, 3),
+            (1, 5),
+            (5, 4),
+            (2, 4),
+            (4, 2),
+            (3, 6)
+        ]
 
         cursor.executemany(sql, val)
         connection.commit()
@@ -139,4 +208,4 @@ def execute_query_users(connection):
         print(f"The error '{e}' occurred")
 
 
-execute_query_users(connection)
+execute_query_likes(connection)
