abcea36fcc14098095caf1ba187adb685f8d66d3
Sanqn <sandisvert@gmail.com>
2022-06-06 22:51:56 +0300

    Mysql insert values

--- SQLBD.py
+++ SQLBD.py
@@ -33,6 +33,8 @@ def create_connection(host_name, user_name, user_password, db_name):
 
 
 connection = create_connection("localhost", "admin", "root", "new_app")
+
+
 #
 #
 # # def create_database(connection):
@@ -47,73 +49,94 @@ connection = create_connection("localhost", "admin", "root", "new_app")
 # # create_database(connection)
 #
 #
-def execute_query(connection, query):
+# def execute_query(connection, query):
+#     cursor = connection.cursor()
+#     try:
+#         cursor.execute(query)
+#         connection.commit()
+#         print("Query executed successfully")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#
+# create_users_table = """
+# CREATE TABLE IF NOT EXISTS users (
+#   id INT AUTO_INCREMENT,
+#   name TEXT NOT NULL,
+#   age INT,
+#   gender TEXT,
+#   nationality TEXT,
+#   PRIMARY KEY (id)
+# ) ENGINE = InnoDB
+# """
+#
+# create_posts_table = """
+# CREATE TABLE IF NOT EXISTS posts (
+#   id INT AUTO_INCREMENT,
+#   title TEXT NOT NULL,
+#   description TEXT NOT NULL,
+#   user_id INTEGER NOT NULL,
+#   FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
+#   PRIMARY KEY (id)
+# ) ENGINE = InnoDB
+# """
+#
+# create_comments_table = """
+# CREATE TABLE IF NOT EXISTS comments (
+#   id INT AUTO_INCREMENT,
+#   text TEXT NOT NULL,
+#   user_id INTEGER NOT NULL,
+#   post_id INTEGER NOT NULL,
+#   FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
+#   FOREIGN KEY fk_post_id (post_id) REFERENCES posts(id),
+#   PRIMARY KEY (id)
+# )ENGINE = InnoDB
+# """
+#
+# create_likes_table = """
+# CREATE TABLE IF NOT EXISTS likes (
+#   id INT AUTO_INCREMENT,
+#   user_id INTEGER NOT NULL,
+#   post_id INTEGER NOT NULL,
+#   FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
+#   FOREIGN KEY fk_post_id (post_id) REFERENCES posts(id),
+#   PRIMARY KEY (id)
+# )ENGINE = InnoDB
+# """
+#
+# create_sos_table = """
+# CREATE TABLE IF NOT EXISTS sos (
+#   id INT AUTO_INCREMENT,
+#   user_id INTEGER NOT NULL,
+#   post_id INTEGER NOT NULL,
+#   INDEX fk_user_id (user_id),
+#   INDEX fk_post_id (post_id),
+#   FOREIGN KEY (user_id) REFERENCES users(id),
+#   FOREIGN KEY (post_id) REFERENCES posts(id),
+#   PRIMARY KEY (id)
+# )ENGINE = InnoDB
+# """
+#
+# execute_query(connection, create_sos_table)
+# # execute_query(connection, create_comments_table)
+
+
+def execute_query_users(connection):
     cursor = connection.cursor()
     try:
-        cursor.execute(query)
+        sql = "INSERT INTO users (name, age, gender, nationality) VALUES ( %s, %s, %s, %s )"
+        val = [('James', 25, 'male', 'USA'),
+               ('Leila', 32, 'female', 'France'),
+               ('Brigitte', 35, 'female', 'England'),
+               ('Mike', 40, 'male', 'Denmark'),
+               ('Elizabeth', 21, 'female', 'Canada')
+               ]
+
+        cursor.executemany(sql, val)
         connection.commit()
         print("Query executed successfully")
     except Error as e:
         print(f"The error '{e}' occurred")
 
 
-create_users_table = """
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT,
-  name TEXT NOT NULL,
-  age INT,
-  gender TEXT,
-  nationality TEXT,
-  PRIMARY KEY (id)
-) ENGINE = InnoDB
-"""
-
-create_posts_table = """
-CREATE TABLE IF NOT EXISTS posts (
-  id INT AUTO_INCREMENT,
-  title TEXT NOT NULL,
-  description TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
-  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
-  PRIMARY KEY (id)
-) ENGINE = InnoDB
-"""
-
-create_comments_table = """
-CREATE TABLE IF NOT EXISTS comments (
-  id INT AUTO_INCREMENT,
-  text TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
-  post_id INTEGER NOT NULL,
-  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
-  FOREIGN KEY fk_post_id (post_id) REFERENCES posts(id),
-  PRIMARY KEY (id)
-)ENGINE = InnoDB
-"""
-
-create_likes_table = """
-CREATE TABLE IF NOT EXISTS likes (
-  id INT AUTO_INCREMENT, 
-  user_id INTEGER NOT NULL, 
-  post_id INTEGER NOT NULL, 
-  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id),
-  FOREIGN KEY fk_post_id (post_id) REFERENCES posts(id),
-  PRIMARY KEY (id)
-)ENGINE = InnoDB
-"""
-
-create_sos_table = """
-CREATE TABLE IF NOT EXISTS sos (
-  id INT AUTO_INCREMENT, 
-  user_id INTEGER NOT NULL, 
-  post_id INTEGER NOT NULL, 
-  INDEX fk_user_id (user_id),
-  INDEX fk_post_id (post_id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (post_id) REFERENCES posts(id),
-  PRIMARY KEY (id)
-)ENGINE = InnoDB
-"""
-
-execute_query(connection, create_sos_table)
-# execute_query(connection, create_comments_table)
+execute_query_users(connection)
