0a5ebb64be3aaa1f4fc814b0da9d47f5307ccdc7
Sanqn <sandisvert@gmail.com>
2022-06-07 21:16:22 +0300

    Mongo

---
+++ MongoDB.py
@@ -0,0 +1,40 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/new_app")
+mydb = client["new_app"]
+users = mydb.users
+
+# user = [{
+#     "name": "John",
+#     "age": 15,
+#     "sex": "male",
+#     "nationality": "USA"},
+#     {
+#     "name": "Lili",
+#     "age": 25,
+#     "sex": "female",
+#     "nationality": "Paris"}
+#     ]
+#
+# x = users.insert_many(user)
+
+# for i in users.find():
+#     print(i)
+
+# for i in users.find({}, {"_id": 0, "name": 1, "nationality": 1}):
+#     print(i)
+
+# for i in users.find({"age": 25}):
+#     print(i)
+
+# Например, чтобы найти документы, в которых поле «адрес» начинается с буквы «S»
+# или выше (в алфавитном порядке), используйте модификатор «больше чем» {"$gt": "S"}:
+
+for i in users.find({"age": {"$gt": 16}}):
+    print(i, "$gt----------")
+
+# Чтобы найти только те документы, где поле «адрес» начинается с буквы «S»,
+# используйте регулярное выражение {"$regex": "^S"}:
+
+for i in users.find({"name": {"$regex": "^J"}}):
+    print(i, "$regex--------")
